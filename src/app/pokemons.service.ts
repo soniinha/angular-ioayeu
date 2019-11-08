b4d3113151816976959ac40769c2b1d25dbfc4b4
soniinha <sonia.castro.t0114708@edu.atec.pt>
2019-11-08 21:37:30 +0000

    1 file modified, 1 added

--- src/app/pokemons.service.ts
+++ src/app/pokemons.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of, BehaviorSubject } from 'rxjs';
+import { Poke } from './pokemons';
 
 @Injectable()
 export class PokemonsService {
 
-  private URLAPI = "https://pokeapi.co/api/v2";
+  private apiUrl = "https://pokeapi.co/api/v2";
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient) { 
+  }
 
-    getAlunos(): Observable<Aluno[]>{
-    return this.http.get<Aluno[]>(this.URLAPI);
+  getPoke(): Observable<Poke[]> {
+    // return of(ALUNOS);
+    return this.http
+           .get<Poke[]>(this.apiUrl + '&results=20');
+  }
+
+  getPokeById(id:number): Observable<Poke> {
+    console.log(id);
+    return this.http.get<Poke>(this.apiUrl + '$results=1&page=' + id);
   }
 
 }
---
+++ src/pokemons.ts
@@ -0,0 +1,8 @@
+export class Pokemons {
+  id: number;
+  name: {
+    first: string,
+    last: string
+  };
+  visible: boolean;
+}
